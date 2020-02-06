b2a42e1b036435b5bed88fa139dd21a1b349a073
Matthew Turner <matthew.m.turner@outlook.com>
2020-02-05 22:11:48 -0500

    made huffman encoding work

--- DataStructuresAlgos/DataStructures/Project2/Problem3.py
+++ DataStructuresAlgos/DataStructures/Project2/Problem3.py
@@ -1,7 +1,6 @@
 import sys
 from collections import deque, namedtuple
 from queue import PriorityQueue
-import heapq
 
 class CharNode:
 
@@ -44,13 +43,19 @@ class Node:
         self.freq = freq
 
     def set_freq(self):
-        self.freq = self.left.freq + self.right.freq
+        self.freq = self.left.get_freq() + self.right.get_freq()
 
     def get_freq(self):
         return self.freq
 
     def visit(self):
         self.visited = True
+
+    def __eq__(self, other):
+        return self.get_freq() == other.get_freq()
+
+    def __lt__(self, other):
+        return self.get_freq() < other.get_freq()
 
 def huffman_encoding(data):
     
@@ -62,12 +67,14 @@ def huffman_encoding(data):
 
     # pairs = []
     pqueue = PriorityQueue()
-    q = []
+    # q = []
     for char, ct in char_frequencies.items():
-        pqueue.put((ct, char))
-        q.append((ct, char))
+        node = Node()
+        node.set_char_count(char, ct)
+        pqueue.put((ct, node))
+        # q.append((ct, char))
 
-    q.sort(reverse=True)
+    # q.sort(reverse=True)
     # for char_pair in sorted_pairs:
     #     # node = CharNode(char_pair.char, char_pair.count)
     #     node = Node()
@@ -80,35 +87,35 @@ def huffman_encoding(data):
     while pqueue.qsize() > 1:
         # node_smaller = queue.pop()
         # node_small = queue.pop()
-        node_smaller2 = pqueue.get()
-        node_small2 = pqueue.get()
-        node_smaller3 = q.pop()
-        node_small3 = q.pop()
+        node_smaller = pqueue.get()[1]
+        node_small = pqueue.get()[1]
+        # node_smaller3 = q.pop()
+        # node_small3 = q.pop()
         # node = Node(node_small, node_smaller)
-        node3 = Node()
-        node3.set_left(node_small3)
-        node3.set_right(node_smaller3)
-        node3.set_freq()
-        node2 = Node()
-        node2.set_left(node_small2)
-        node2.set_right(node_smaller2)
-        node2.set_freq()
-        q.append((node.freq, node))
-        queue2.put(node2.freq, node2)
+        # node3 = Node()
+        # node3.set_left(node_small3)
+        # node3.set_right(node_smaller3)
+        # node3.set_freq()
+        node = Node()
+        node.set_left(node_small)
+        node.set_right(node_smaller)
+        node.set_freq()
+        pqueue.put((node.get_freq(), node))
+        # q.append((node.freq, node))
+        # queue2.put(node2.freq, node2)
 
-    root_node = queue.pop()
-    root_node2 = queue2.get()
+    root_node = pqueue.get()[1]
 
-    encoded_string = ""
+    # encoded_string = ""
 
     def pre_order(root):
 
-        chars = []
         codes = {}
 
         def traverse(node, code):
             if node:
-                codes[node] = code
+                if node.char:
+                    codes[node.char] = code
                 code += '0'
                 traverse(node.get_left(), code)
                 code += '1'
@@ -118,17 +125,10 @@ def huffman_encoding(data):
         code = ""        
         traverse(root, code)
 
-        return chars, codes
+        return codes
 
-    chars, codes = pre_order(root_node)
-    char_codes = {}
-
-    for node, code in codes.items():
-        if node.char is not None:
-            char_codes[node.char] = code
-
-    char_codes
-
+    codes = pre_order(root_node)
+    
 
 def huffman_decoding(data,tree):
     pass
