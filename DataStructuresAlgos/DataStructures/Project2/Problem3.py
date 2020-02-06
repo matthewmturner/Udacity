3ef19083a21637f283832031cd18918228fe0a8f
Matthew Turner <matthew.m.turner@outlook.com>
2020-02-05 23:51:07 -0500

    encoding and decoding updates

--- DataStructuresAlgos/DataStructures/Project2/Problem3.py
+++ DataStructuresAlgos/DataStructures/Project2/Problem3.py
@@ -65,48 +65,23 @@ def huffman_encoding(data):
     for char in data:
         char_frequencies[char] = char_frequencies.get(char, 0) + 1
 
-    # pairs = []
     pqueue = PriorityQueue()
-    # q = []
+
     for char, ct in char_frequencies.items():
         node = Node()
         node.set_char_count(char, ct)
         pqueue.put((ct, node))
-        # q.append((ct, char))
-
-    # q.sort(reverse=True)
-    # for char_pair in sorted_pairs:
-    #     # node = CharNode(char_pair.char, char_pair.count)
-    #     node = Node()
-    #     node.set_char_count(char_pair.char, char_pair.count)
-
-    #     print(f"Node ({node.char}): {node.freq}")
-    #     queue.appendleft(node)
-    #     queue2.put((node, node.freq))
-
+    
     while pqueue.qsize() > 1:
-        # node_smaller = queue.pop()
-        # node_small = queue.pop()
         node_smaller = pqueue.get()[1]
         node_small = pqueue.get()[1]
-        # node_smaller3 = q.pop()
-        # node_small3 = q.pop()
-        # node = Node(node_small, node_smaller)
-        # node3 = Node()
-        # node3.set_left(node_small3)
-        # node3.set_right(node_smaller3)
-        # node3.set_freq()
         node = Node()
         node.set_left(node_small)
         node.set_right(node_smaller)
         node.set_freq()
         pqueue.put((node.get_freq(), node))
-        # q.append((node.freq, node))
-        # queue2.put(node2.freq, node2)
 
     root_node = pqueue.get()[1]
-
-    # encoded_string = ""
 
     def pre_order(root):
 
@@ -121,27 +96,49 @@ def huffman_encoding(data):
                 code += '1'
                 traverse(node.get_right(), code)
         
-        
-        code = ""        
-        traverse(root, code)
+        code = "0"        
+        traverse(root.get_left(), code)
+        code = "1"        
+        traverse(root.get_right(), code)
 
         return codes
 
     codes = pre_order(root_node)
-    
+
+    encoded_data = ""
+    for char in data:
+        encoded_data += codes[char]
+
+    return encoded_data, root_node, codes
 
 def huffman_decoding(data,tree):
-    pass
+
+    node = tree
+    decoded_string = ""
+
+    for char in data:
+        if char == '0':
+            node = node.get_left()
+        elif char == '1':
+            node = node.get_right()
+
+        if node.char:
+            decoded_string += node.char
+            node = tree    
+
+    return decoded_string
+
 
 if __name__ == "__main__":
     codes = {}
 
     a_great_sentence = "The bird is the word"
+    a_great_sentence2 = "THE BIRD IS THE WORD"
 
-    print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
-    print ("The content of the data is: {}\n".format(a_great_sentence))
+    print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence2)))
+    print ("The content of the data is: {}\n".format(a_great_sentence2))
 
-    encoded_data, tree = huffman_encoding(a_great_sentence)
+    encoded_data, tree, codes = huffman_encoding(a_great_sentence2)
 
     print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
     print ("The content of the encoded data is: {}\n".format(encoded_data))
