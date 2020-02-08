8d0b143445c1b64010750fa620f07c65ad5db3bb
Matthew Turner <matthew.m.turner@outlook.com>
2020-02-08 00:26:41 -0500

    formatting, docstrings and cleanup

--- DataStructuresAlgos/DataStructures/Project2/Problem2.py
+++ DataStructuresAlgos/DataStructures/Project2/Problem2.py
@@ -1,7 +1,18 @@
 import os
 
+
 def find_files(suffix, path):
+    """Main function to search diretory recursively for files with certain path suffix.
+    
+    Arguments:
+        suffix {str} -- File type
+        path {str} -- Path to start looking for files
+    
+    Returns:
+        [list] -- List of all files with provided suffix
+    """
     return recursive_find_files(suffix, path)
+
 
 def recursive_find_files(suffix, path):
     """
@@ -24,7 +35,7 @@ def recursive_find_files(suffix, path):
 
     for item in os.listdir(path):
         full_path = os.path.join(path, item)
-        if os.path.isfile(full_path) and item.endswith(".c"):
+        if os.path.isfile(full_path) and item.endswith(suffix):
             files.append(full_path)
         elif os.path.isdir(full_path):
             results = recursive_find_files(suffix, full_path)
@@ -33,6 +44,25 @@ def recursive_find_files(suffix, path):
     return files
 
 
-if __name__ == '__main__':
-    res = find_files(".c", r"C:\Users\matth\OneDrive\Learning\Udacity\DataStructuresAlgos\DataStructures\Project2\testdir")
-    print(res)
+if __name__ == "__main__":
+    print(
+        find_files(
+            ".c",
+            r"C:\Users\matth\OneDrive\Learning\Udacity\DataStructuresAlgos\DataStructures\Project2\testdir",
+        )
+    )
+    # Expected output: List of all files in path ending in .c
+    print(
+        find_files(
+            "",
+            r"C:\Users\matth\OneDrive\Learning\Udacity\DataStructuresAlgos\DataStructures\Project2\testdir",
+        )
+    )
+    # Expected output: List of all files in path
+    print(
+        find_files(
+            "abcdef",
+            r"C:\Users\matth\OneDrive\Learning\Udacity\DataStructuresAlgos\DataStructures\Project2\testdir",
+        )
+    )
+    # Expected output: Empty list
