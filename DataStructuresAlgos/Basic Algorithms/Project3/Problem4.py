3e76452e02cbe84536d946a7553df73c28375d6c
Matthew Turner <matthew.m.turner@outlook.com>
2020-02-18 22:19:41 -0500

    problem 4 solution

---
+++ DataStructuresAlgos/Basic Algorithms/Project3/Problem4.py
@@ -0,0 +1,39 @@
+def sort_012(input_list):
+    """
+    Given an input array consisting on only 0, 1, and 2, sort the array in a single traversal.
+
+    Args:
+       input_list(list): List to be sorted
+    """
+    zeros = []
+    ones = []
+    twos = []
+
+    sorted_list = []
+    for n in input_list:
+        if n == 0:
+            zeros.append(0)
+        elif n == 1:
+            ones.append(1)
+        elif n == 2:
+            twos.append(2)
+
+    sorted_list = zeros + ones + twos
+
+    return sorted_list
+
+
+def test_function(test_case):
+    sorted_array = sort_012(test_case)
+    print(sorted_array)
+    if sorted_array == sorted(test_case):
+        print("Pass")
+    else:
+        print("Fail")
+
+
+test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
+test_function(
+    [2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1]
+)
+test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
