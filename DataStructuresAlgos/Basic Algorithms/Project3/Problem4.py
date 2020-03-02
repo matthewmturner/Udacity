a3742d7a7517a7390a545f20286a41bc32a28e87
Matthew Turner <matthew.m.turner@outlook.com>
2020-03-01 20:35:20 -0500

    add p4 analysis

--- DataStructuresAlgos/Basic Algorithms/Project3/Problem4.py
+++ DataStructuresAlgos/Basic Algorithms/Project3/Problem4.py
@@ -1,10 +1,13 @@
 def sort_012(input_list):
     """
-    Given an input array consisting on only 0, 1, and 2, sort the array in a single traversal.
+    Given an input array consisting of only 0, 1, and 2, sort the array in a single traversal.
 
     Args:
        input_list(list): List to be sorted
     """
+    if not isinstance(input_list, list):
+        raise ValueError("Input must be a list")
+
     zeros = []
     ones = []
     twos = []
@@ -17,6 +20,8 @@ def sort_012(input_list):
             ones.append(1)
         elif n == 2:
             twos.append(2)
+        else:
+            raise ValueError("List must only have intgers of 0, 1, or 2")
 
     sorted_list = zeros + ones + twos
 
@@ -25,7 +30,6 @@ def sort_012(input_list):
 
 def test_function(test_case):
     sorted_array = sort_012(test_case)
-    print(sorted_array)
     if sorted_array == sorted(test_case):
         print("Pass")
     else:
@@ -33,7 +37,17 @@ def test_function(test_case):
 
 
 test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
-test_function(
-    [2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1]
-)
+# Expected output: pass
+test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
+# Expected output: pass
 test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+# Expected output: pass
+test_function([1, 1, 1, 1, 1, 1, 1])
+# Expected output: pass
+test_function([1])
+# Expected output: pass
+test_function(120)
+# Expected output: ValueError input must be a list
+# Need to comment out above function to see error for below
+test_function([1, 3])
+# Expected output: ValueError List must only have integers of 0, 1, or 2
